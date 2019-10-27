874f254a325fdebf2fab86e37deb2285efc5f6bb
Valeria Serci <balerty84@gmail.com>
2019-10-27 20:10:48 +0000

    Add tests for Backstage passes

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -13,7 +13,7 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].sell_in).to eq(0)
     end
-    context 'quality tests' do
+    context '#quality' do
       it "tests normal item" do
         items = [Item.new("foo", 20, 10)]
         GildedRose.new(items).update_quality()
@@ -50,10 +50,35 @@ describe GildedRose do
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq(50)
       end
-      it "tests item quality of 50 stays 50" do
+      it "tests item 1 quality of 50 stays 50" do
         items = [Item.new("Aged Brie", 10, 50)]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq(50)
+      end
+      it "tests item 2 quality of 50 stays 50" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 50)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq(50)
+      end
+      it "tests Backstage passes" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 20, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq(11)
+      end
+      it "tests Backstage passes after 10 or less days" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq(12)
+      end
+      it "tests Backstage passes after 5 or less days" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq(13)
+      end
+      it "tests expired Backstage passes" do
+        items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        GildedRose.new(items).update_quality()
+        expect(items[0].quality).to eq(0)
       end
 
     end
